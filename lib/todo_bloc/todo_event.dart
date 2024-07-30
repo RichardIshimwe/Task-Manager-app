a446f98e70e5a72ab7e3c29119ee5e1f7e0e267f
RichardIshimwe <ishimwerichard26@gmail.com>
2024-07-30 12:24:09 +0200

    chore : todo events setup

--- lib/todo_bloc/todo_event.dart
+++ lib/todo_bloc/todo_event.dart
@@ -1,8 +1,37 @@
 part of 'todo_bloc.dart';
 
-sealed class TodoEvent extends Equatable {
+abstract class TodoEvent extends Equatable {
   const TodoEvent();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
+}
+
+class TodoStated extends TodoEvent {}
+
+class AddTodo extends TodoEvent {
+  final Todo todo;
+
+  const AddTodo({required this.todo});
+
+  @override
+  List<Object> get props => [todo];
+}
+
+class RemoveTodo extends TodoEvent {
+  final Todo todo;
+
+  const RemoveTodo({required this.todo});
+
+  @override
+  List<Object> get props => [todo];
+}
+
+class AlterTodo extends TodoEvent {
+  final int index;
+
+  const AlterTodo({required this.index});
+
+  @override
+  List<Object> get props => [index];
 }
